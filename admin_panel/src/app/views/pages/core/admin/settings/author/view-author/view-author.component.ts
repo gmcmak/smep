6bb7b934711b9ef94a8c09bf038aaf893e385881
deshan <sbdeshan@gmail.com>
2017-12-19 10:08:19 +0530

    view author

--- admin_panel/src/app/views/pages/core/admin/settings/author/view-author/view-author.component.ts
+++ admin_panel/src/app/views/pages/core/admin/settings/author/view-author/view-author.component.ts
@@ -39,7 +39,27 @@ export class ViewAuthorComponent implements OnInit{
 
     private getAuthors(){
         this.AuthorService.getAuthorsList()
-            .subscribe(success=>{this.authorList = success.success});
+            .subscribe(
+                success=>{
+                    this.authorList = success.success;
+                    $("#authorsTable").find('tbody').empty();
+                    var dataClaims = this.authorList;
+                    for (let i = 0; i < dataClaims.length; i++) {
+                        $('#authorsTable').dataTable().fnAddData([
+                            (i + 1),
+                            dataClaims[i].name,
+                            dataClaims[i].registration_number,
+                            dataClaims[i].registered_date,
+                            dataClaims[i].email,
+                            dataClaims[i].contact_number,
+                            dataClaims[i].address,
+                            '<label class="switch"><input type= "checkbox" value= "' + dataClaims[i].status + '" ><span class="slider round" > </span></label>',
+                            '<a [routerLink]="[' + "'" + "../../institute/update-institute" + "'" + ']"' + ' class="fa fa-1x fa-pencil-square-o"></a>',
+                            '<a data-toggle="modal" data-target="#deleteModal"><li class="fa  fa-1x fa-trash"></li></a>'
+                        ]);
+                    }
+                }
+            );
     }
     
 }
