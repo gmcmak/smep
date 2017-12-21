3f8dce45f064a6a898c02fee33be3330d83faae7
deshan <sbdeshan@gmail.com>
2017-12-21 16:38:05 +0530

    update consumer modify

--- admin_panel/src/app/views/pages/core/admin/content-consumer/update-consumer/update-consumer.component.ts
+++ admin_panel/src/app/views/pages/core/admin/content-consumer/update-consumer/update-consumer.component.ts
@@ -24,13 +24,9 @@ export class UpdateConsumersComponent implements OnInit{
     public modulesList;
     private permissions;
 
-    public editId = 2; //consumer id for edit
+    public editId = 91; //consumer id for edit
     public editConsumerList;
-<<<<<<< HEAD
-    public moduleId = new Array();
-=======
     public moduleId: any[];
->>>>>>> b29a7be61f9a7f5d8ff9c3473ec14b9819332f05
 
     public consumerAddingStatus;
 
@@ -118,7 +114,6 @@ export class UpdateConsumersComponent implements OnInit{
                 for(let i=0 ; i<this.editConsumerList[0].modules.length; i++){
                     this.moduleId.push(this.editConsumerList[0].modules[i].id);
                 }
-                alert(this.moduleId);
             }
             
         );
