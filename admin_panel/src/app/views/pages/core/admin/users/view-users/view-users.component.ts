fff7b0483981ad63ba1d663193e6c2bc531524e6
Gayan <gayan2255@gmail.com>
2017-12-29 09:14:06 +0530

    user list view fixed

--- admin_panel/src/app/views/pages/core/admin/users/view-users/view-users.component.ts
+++ admin_panel/src/app/views/pages/core/admin/users/view-users/view-users.component.ts
@@ -10,6 +10,7 @@ import { UserService } from "../../../../../../services/businessservices/core/us
 
 import { FormGroup, Validators, FormControl} from '@angular/forms';
 import { LocalStorageStore } from '../../../../../../services/storage/local-storage.service';
+
 declare var $: any;
 declare var jQuery: any;
 
@@ -34,20 +35,23 @@ export class ViewUsersComponent implements OnInit{
         private UserService:UserService,
         private localStorageService: LocalStorageStore,
         private activatedRoute: ActivatedRoute
-    ) {}    
+    ) {
+
+        //
+    }    
 
     ngOnInit(): void {
         this.loggedInUserList = JSON.parse(this.localStorageService.get('userData'));
-        this.getUsers();        
-        this.dataTable();
-    }
- 
-    dataTable(){
-        $('#dataTableUsers').DataTable({
-            "language": {
-                "search": "Search by: (Name/ Email/ Role)"
+        this.getUsers();    
+        setTimeout(
+            function(){
+                $('#dataTableUsers').DataTable({
+                    "language": {
+                        "search": "Search by: (Name/ Email/ Role)"
+                    }
+                });                
             }
-        });
+            , 2000);          
     }
  
     /**
@@ -59,21 +63,10 @@ export class ViewUsersComponent implements OnInit{
             .subscribe( 
                 success => {
                     this.usersList = success.success;
-                    $("#dataTableUsers").find('tbody').empty(); 
-                    var dataClaims = this.usersList;
-                    for (let i = 0; i < dataClaims.length; i++) {
-                        $('#dataTableUsers').dataTable().fnAddData([
-                            (i+1),
-                            dataClaims[i].name,
-                            dataClaims[i].email,
-                            dataClaims[i].role.name,
-                            '<a [routerLink]="['+"../../users/update-users"+']"'+' class="fa fa-1x fa-pencil-square-o"></a>',
-                            '<a data-toggle="modal" data-target="#deleteModal"><li class="fa  fa-1x fa-trash"></li></a>'
-                        ]);
-                     }
                 }
             );      
     }        
 
 
 }
+
