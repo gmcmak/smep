d770f48e134f89e3526093cc57be98180b8c0916
Gayan <gayan2255@gmail.com>
2018-01-11 10:57:26 +0530

    Update Model relationship after add submission and content

--- smepadmin/app/Keyword.php
+++ smepadmin/app/Keyword.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Keyword extends Model
 {
-    //
+    public function content()
+    {
+        return $this->belongsToMany('App\Content');
+    }  
 }
--- smepadmin/app/submission.php
+++ smepadmin/app/submission.php
@@ -4,7 +4,10 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class submission extends Model
+class Submission extends Model
 {
-    //
+    protected $fillable = [
+        'name', 'url', ' user_id', 'status', 'level'
+    ];
+
 }
