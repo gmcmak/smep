a803a192611adbe20f43aeef4b3fa174d80ad74c
deshan <sbdeshan@gmail.com>
2018-01-16 15:26:47 +0530

    single submission form interface

--- admin_panel/src/app/views/pages/core/content-provider/cp-submissions/single-submission/single-submission.component.ts
+++ admin_panel/src/app/views/pages/core/content-provider/cp-submissions/single-submission/single-submission.component.ts
@@ -6,6 +6,10 @@ import { KeywordService } from "../../../../../../services/businessservices/core
 
 const URL_REGEX = ('https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9]\.[^\s]{2,}');
 
+declare var $:any;
+declare var jQuery:any;
+
+
 @Component({
     selector: 'cp-single-submission',
     templateUrl: 'single-submission.component.html',
@@ -21,6 +25,20 @@ export class SingleSubmissionComponent implements OnInit{
     public exploreList;
     public keywordList;
 
+    public keywordItemList = new Array();
+    selectedKeyword = [];
+    keyword_setting = {};
+
+    public CategoryItemList = new Array();
+    selectedCategory = [];
+    category_setting = {};
+
+    public ExploreItemList = new Array();
+    selectedExplore = [];
+    explore_setting = {};
+
+
+
     constructor(
         private formBuilder:FormBuilder,
         private categoryService: CategoryService,
@@ -33,19 +51,98 @@ export class SingleSubmissionComponent implements OnInit{
         this.getCategories();
         this.getExplores();
         this.getKeywords();
+
+
+        //for keyword drop down
+        this.keyword_setting = {
+            text: "Select Keyword",
+            selectAllText: 'Select All',
+            unSelectAllText: 'UnSelect All',
+            // classes: "myclass custom-class"
+        };
+
+        //for category drop down
+        this.category_setting = {
+            text: "Select Category",
+            selectAllText: 'Select All',
+            unSelectAllText: 'UnSelect All',
+            // classes: "myclass custom-class"
+        };
+
+        //for explore drop down
+        this.explore_setting = {
+            text: "Select Explore",
+            selectAllText: 'Select All',
+            unSelectAllText: 'UnSelect All',
+            // classes: "myclass custom-class"
+        };
+    }
+
+    //keyword drop down list functions
+    onItemSelect1(item: any) {
+        //console.log(item);
+        console.log(this.selectedKeyword);
+    }
+    OnItemDeSelect1(item: any) {
+        //console.log(item);
+        console.log(this.selectedKeyword);
+    }
+    onSelectAll1(items: any) {
+        console.log(items);
+    }
+    onDeSelectAll1(items: any) {
+        console.log(items);
+    }
+
+    //category drop down list functions
+    onItemSelect2(item: any) {
+        //console.log(item);
+        console.log(this.selectedCategory);
+    }
+    OnItemDeSelect2(item: any) {
+        //console.log(item);
+        console.log(this.selectedCategory);
+    }
+    onSelectAll2(items: any) {
+        console.log(items);
+    }
+    onDeSelectAll2(items: any) {
+        console.log(items);
+    }
+
+    //explore drop down list functions
+    onItemSelect3(item: any) {
+        //console.log(item);
+        console.log(this.selectedExplore);
+    }
+    OnItemDeSelect3(item: any) {
+        //console.log(item);
+        console.log(this.selectedExplore);
+    }
+    onSelectAll3(items: any) {
+        console.log(items);
+    }
+    onDeSelectAll3(items: any) {
+        console.log(items);
     }
 
     private initializeSingleForm(): void{
         this.singleSubForm = this.formBuilder.group({
-            'sub_url': [null, [Validators.required, Validators.pattern(URL_REGEX)]],
-            'sub_type': [null, [Validators.required]],
-            'sub_title': [null, [Validators.required]], 
-            'sub_video_url': [null, [Validators.pattern(URL_REGEX)]],
-            'sub_keyword': [null, [Validators.required]],
-            'sub_free': [null],
-            'sub_category': [null, [Validators.required]],
-            'sub_explore': [null, [Validators.required]],
-            'sub_description': [null]
+            'sub_url': ['', [Validators.required, Validators.pattern(URL_REGEX)]],
+            'sub_type': ['', [Validators.required]],
+            'sub_title': ['', [Validators.required]], 
+            'sub_video_url': ['', [Validators.pattern(URL_REGEX)]],
+            'sub_keywords': new FormGroup({
+                'sub_keyword': new FormControl('', Validators.required),
+                'sub_free': new FormControl('')
+            }),
+            'sub_categories': new FormGroup({
+                'sub_category': new FormControl('', Validators.required)
+            }),
+            'sub_explores': new FormGroup({
+                'sub_explore': new FormControl('', Validators.required)
+            }),
+            'sub_description': ['']
         });
     }
 
@@ -66,8 +163,14 @@ export class SingleSubmissionComponent implements OnInit{
     public getCategories(){
         this.categoryService.getCategoriesList().subscribe(
             success => {
-                this.categoryList = success.success
+                this.categoryList = success.success;
+                for (let i = 0; i < this.categoryList.length; i++) {
+                    this.CategoryItemList[i] = new Array();
+                    this.CategoryItemList[i]['id'] = this.categoryList[i].id;
+                    this.CategoryItemList[i]['itemName'] = this.categoryList[i].en_name;  
+                }
             }
+               
         );
     }
 
@@ -77,7 +180,12 @@ export class SingleSubmissionComponent implements OnInit{
     public getExplores(){
         this.exploreService.getExploresList().subscribe(
             success => {
-                this.exploreList = success.success
+                this.exploreList = success.success;
+                for (let i = 0; i < this.exploreList.length; i++) {
+                    this.ExploreItemList[i] = new Array();
+                    this.ExploreItemList[i]['id'] = this.exploreList[i].id;
+                    this.ExploreItemList[i]['itemName'] = this.exploreList[i].en_tag;
+                }
             }
         );
     }
@@ -88,11 +196,19 @@ export class SingleSubmissionComponent implements OnInit{
     public getKeywords(){
         this.keywordService.getKeywordList().subscribe(
             success => {
-                this.keywordList = success.success
+                this.keywordList = success.success;
+                for(let i=0; i<this.keywordList.length; i++){  
+                    this.keywordItemList[i] = new Array();
+                    this.keywordItemList[i]['id'] = this.keywordList[i].id;
+                    this.keywordItemList[i]['itemName'] = this.keywordList[i].en_name;
+                }
             }
         );
     }
 
+    public displayValue(formData){
+        // console.log('Input Data '+ this.singleSubmission.selectedCategory);
+    }
 }
 
 export class SingleSubmission{
@@ -100,9 +216,9 @@ export class SingleSubmission{
     public sub_type1: number;
     public sub_title1: string;
     public sub_video_url1: string;
-    public sub_Keyword1: number;
+    public selectedKeyword: number;
     public sub_free1: string;
-    public sub_category1: number;
-    public sub_explore1: number;
+    public selectedCategory: number;
+    public selectedExplore: number;
     public sub_description1: string;
 }
